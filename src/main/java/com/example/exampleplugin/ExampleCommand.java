856eb59ec360adaabc62b4929706c73108946552
Mat Sharp <167991473+Mat-Sharp0@users.noreply.github.com>
2026-07-27 10:17:47 +0200

    Replace deprecated setPermissionGroups(GameMode.Adventure) to setPermissionGroups("hytale:Adventurer")

--- src/main/java/com/example/exampleplugin/ExampleCommand.java
+++ src/main/java/com/example/exampleplugin/ExampleCommand.java
@@ -16,7 +16,7 @@ public class ExampleCommand extends CommandBase {
 
     public ExampleCommand(String pluginName, String pluginVersion) {
         super("test", "Prints a test message from the " + pluginName + " plugin.");
-        this.setPermissionGroup(GameMode.Adventure); // Allows the command to be used by anyone, not just OP
+        this.setPermissionGroups("hytale:Adventurer"); // Allows the command to be used by anyone, not just OP
         this.pluginName = pluginName;
         this.pluginVersion = pluginVersion;
     }
